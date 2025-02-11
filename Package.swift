b341f10960a5b4700ee69338639ced02c0d5f797
Bayram Yelec <bayrammyelec@gmail.com>
2025-02-11 10:58:18 +0300

    commit

--- Package.swift
+++ Package.swift
@@ -7,16 +7,16 @@ let package = Package(
     name: "BYSwiftButton",
     platforms: [.iOS(.v15)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "BYSwiftButton",
-            targets: ["BYSwiftButton"]),
+            targets: ["BYSwiftButton"]
+        ),
     ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "BYSwiftButton"),
-
+            name: "BYSwiftButton",
+            dependencies: []
+        ),
     ]
 )
